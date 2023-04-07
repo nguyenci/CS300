9d285b16fe0e4023a0f51d36d0b001447e930b9c
Cinthya <100801333+nguyenci@users.noreply.github.com>
2023-04-06 19:01:45 -0500

    Create Attribute.java

---
+++ P08 Linked Sorting/Attribute.java
@@ -0,0 +1,38 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    P08 LinkedBookshelf, Attribute
+// Course:   CS 300 Fall 2021
+//
+// Author:   Cinthya Nguyen
+// Email:    cnguyen37@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:    N/A
+// Partner Email:   N/A
+// Partner Lecturer's Name: N/A
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   ___ Write-up states that pair programming is allowed for this assignment.
+//   ___ We have both read and understand the course Pair Programming Policy.
+//   ___ We have registered our team prior to the team registration deadline.
+//
+///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
+//
+// Persons:         N/A
+// Online Sources:  N/A
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @author Cinthya Nguyen
+ *
+ */
+/**
+ * This enumeration groups the names of constants representing the sortable attributes of Book
+ * objects.
+ */
+public enum Attribute {
+  TITLE, AUTHOR, PAGECOUNT, ID;
+}
